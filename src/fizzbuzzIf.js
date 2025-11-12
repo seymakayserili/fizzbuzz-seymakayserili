5d66a1ede1cf7f5ab950ba72bc8320b433d098f3
seymakayserili <seymakayserili@gmail.com>
2025-11-12 17:52:58 +0300

    Tüm testler başarıyla geçti

--- src/fizzbuzzIf.js
+++ src/fizzbuzzIf.js
@@ -1,5 +1,13 @@
-function fizzbuzzIf(num) {
-    // Kodlar buraya gelecek
+function fizzbuzzIf(number) {
+    if (number % 3 === 0 && number % 5 === 0) {
+        return 'FizzBuzz';
+    } else if (number % 3 === 0) {
+        return 'Fizz';
+    } else if (number % 5 === 0) {
+        return 'Buzz';
+    } else {
+        return number;
+    }
 }
 
 module.exports = fizzbuzzIf;
--- src/fizzbuzzSwitch.js
+++ src/fizzbuzzSwitch.js
@@ -1,5 +1,14 @@
-function fizzbuzzSwitch(num) {
-    // Kodlar buraya gelecek
+function fizzbuzzSwitch(number) {
+    switch (true) {
+        case (number % 3 === 0 && number % 5 === 0):
+            return 'FizzBuzz';
+        case (number % 3 === 0):
+            return 'Fizz';
+        case (number % 5 === 0):
+            return 'Buzz';
+        default:
+            return number;
+    }
 }
 
 module.exports = fizzbuzzSwitch;
